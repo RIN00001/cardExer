74132f46b4b6f0857bd3eb42748ee1786602604e
RIN00001 <hamerboy37@gmail.com>
2025-10-02 19:07:13 +0700

    Soal 2 First Version

--- app/src/main/java/com/example/cardexer/Soal2/UserInterface2.kt
+++ app/src/main/java/com/example/cardexer/Soal2/UserInterface2.kt
@@ -32,13 +32,11 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
